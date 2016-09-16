b0b0d82ca0ac3424a5dc94a23f2c81ea5362ab9a
gustavekd <gustav.ekelund99@gmail.com>
2016-09-16 11:40:09 +0200

    sds

--- If-satser/Program.cs
+++ If-satser/Program.cs
@@ -8,8 +8,33 @@ namespace If_satser
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
+
+            //övning1
         {
+            Console.WriteLine("Skriv in mönsterjup");
+            int mönsterdjup = int.Parse(Console.ReadLine());
+
+            if (mönsterdjup  < 1.6)
+            {
+                Console.WriteLine("olagligt däck!");
+            }
+
+            //övning2
+
+            Console.WriteLine("Mata in ålder");
+            int ålder = int.Parse(Console.ReadLine);
+
+            if (ålder < 12)
+            {
+                Console.WriteLine.("vit");
+            }
+            else (ålder < 18)
+                    {
+                Console.WriteLine.("grön");
+
+           }
+
         }
     }
 }
